c4d125a25cda67946df6087b912bc0b44a82a917
The Ducktor <theductor@duck.com>
2025-01-18 09:02:47 +0800

    Initial Commit

---
+++ QuickDMG/QuickDMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDMGApp.swift
+//  QuickDMG
+//
+//  Created by Jackson Powell on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickDMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickDMGTests/QuickDMGTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDMGTests.swift
+//  QuickDMGTests
+//
+//  Created by Jackson Powell on 1/18/25.
+//
+
+import Testing
+@testable import QuickDMG
+
+struct QuickDMGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
